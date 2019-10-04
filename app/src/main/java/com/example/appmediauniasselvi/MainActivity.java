49b68765b5130c18d7a414c25b0f5f4c305f7f06
clebernascimento <clebertds@gmail.com>
2019-10-04 18:39:50 -0400

    acao do click do button calcular

--- app/src/main/java/com/example/appmediauniasselvi/MainActivity.java
+++ app/src/main/java/com/example/appmediauniasselvi/MainActivity.java
@@ -3,12 +3,57 @@ package com.example.appmediauniasselvi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText avaliacao1;
+    private EditText avaliacao2;
+    private EditText discussiva;
+    private EditText avaliacaoFinal;
+
+    private TextView textMedia;
+    private TextView textmediaFinal;
+    private TextView textresulatado;
+
+    private Button limpar;
+    private Button calcular;
+
+    private double media, result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        avaliacao1 = findViewById(R.id.avaliacao1);
+        avaliacao2 = findViewById(R.id.avaliacao2);
+        discussiva = findViewById(R.id.discussiva);
+        avaliacaoFinal =findViewById(R.id.avaliacaoFinal);
+
+        textMedia = findViewById(R.id.textMedia);
+        textmediaFinal = findViewById(R.id.textMedia_Final);
+        textresulatado = findViewById(R.id.textResultado);
+
+        calcular = findViewById(R.id.buttonCalcular);
+        calcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double nota1 = Double.parseDouble(avaliacao1.getText().toString());
+                double nota2 = Double.parseDouble(avaliacao2.getText().toString());
+                double nota3 = Double.parseDouble(discussiva.getText().toString());
+                double nota4 = Double.parseDouble(avaliacaoFinal.getText().toString());
+
+                media = ((nota1 * 1.5)+(nota2 *1.5)+(nota3 * 4)+(nota4 * 3))/10;
+
+                if (media <= 6.50)
+                {
+                    textMedia.setText("Média: " + media);
+                }
+            }
+        });
     }
 }
