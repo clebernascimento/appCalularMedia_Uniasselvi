ac05ff2eb4d040443b10d4b3edad1238f969d528
clebernascimento <clebertds@gmail.com>
2019-10-10 18:22:21 -0400

    refactory MainActivity

--- app/src/main/java/com/example/appmediauniasselvi/MainActivity.java
+++ app/src/main/java/com/example/appmediauniasselvi/MainActivity.java
@@ -1,21 +1,13 @@
 package com.example.appmediauniasselvi;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
-import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.Toolbar;
 
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity implements
         MediaFragment.OnFragmentInteractionListener,
