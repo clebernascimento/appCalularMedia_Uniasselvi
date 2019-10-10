7182879ceb3dbef02206b26c146050a911cdf856
clebernascimento <clebertds@gmail.com>
2019-10-10 12:41:59 -0400

    conflicts init Fragment

--- app/src/main/java/com/example/appmediauniasselvi/MediaFragment.java
+++ app/src/main/java/com/example/appmediauniasselvi/MediaFragment.java
@@ -1,6 +1,7 @@
 package com.example.appmediauniasselvi;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -9,32 +10,39 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 
-
- */
-public class MediaFragmant extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class MediaFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
+    private EditText avaliacao1;
+    private EditText avaliacao2;
+    private EditText discussiva;
+    private EditText avaliacaoFinal;
 
-    public MediaFragmant() {
+    private TextView textMedia;
+    private TextView textmediaFinal;
+    private TextView textresulatado;
+
+    private Button limpar;
+    private Button calcular;
+
+    private double media;
+
+    public MediaFragment() {
         // Required empty public constructor
     }
 
 
-    public static MediaFragmant newInstance(String param1, String param2) {
-        MediaFragmant fragment = new MediaFragmant();
+    public static MediaFragment newInstance(String param1, String param2) {
+        MediaFragment fragment = new MediaFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,14 +57,111 @@ public class MediaFragmant extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_media_fragmant, container, false);
+        final View view = inflater.inflate(R.layout.fragment_media_fragmant, container, false);
+        avaliacao1 = (EditText)view.findViewById(R.id.avaliacao1);
+        avaliacao2 = (EditText)view.findViewById(R.id.avaliacao2);
+        discussiva = (EditText)view.findViewById(R.id.discussiva);
+        avaliacaoFinal = (EditText)view.findViewById(R.id.avaliacaoFinal);
+
+        textMedia = (TextView) view.findViewById(R.id.textMedia);
+        textmediaFinal = (TextView) view.findViewById(R.id.textMedia_Final);
+        textresulatado = (TextView) view.findViewById(R.id.textResultado);
+
+        limpar = (Button) view.findViewById(R.id.buttonLimpar);
+        limpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                limparCampos();
+            }
+        });
+
+
+        calcular = view.findViewById(R.id.buttonCalcular);
+        calcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calcularMedia();
+            }
+        });
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+    public void calcularMedia(){
+
+        DecimalFormat formatar = new DecimalFormat("0.00");
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols();
+        symbols.setGroupingSeparator('.');
+
+        if ((avaliacao1.getText().toString().isEmpty()) || (avaliacao2.getText().toString().isEmpty()) ||
+                (discussiva.getText().toString().isEmpty()) || (avaliacaoFinal.getText().toString().isEmpty())) {
+            Toast.makeText(getActivity(), "Todos os campos devem ser preenchidos", Toast.LENGTH_LONG).show();
+        } else {
+            double nota1 = Float.parseFloat(avaliacao1.getText().toString());
+            double nota2 = Float.parseFloat(avaliacao2.getText().toString());
+            double nota3 = Float.parseFloat(discussiva.getText().toString());
+            double nota4 = Float.parseFloat(avaliacaoFinal.getText().toString());
+            media = ((nota1 * 1.5) + (nota2 * 1.5) + (nota3 * 4) + (nota4 * 3)) / 10;
         }
+        if (media <= 6.50) {
+            textMedia.setText("Média: " + formatar.format(media));
+            textmediaFinal.setText("Média: " + formatar.format(media));
+            textresulatado.setText("Reprovado !");
+            textresulatado.setTextColor(Color.RED);
+        }
+        if ((media >= 6.51) && (media <= 7.50)) {
+            textMedia.setText("Média: " + formatar.format(media));
+            textmediaFinal.setText("Média Final: 7.00");
+            textresulatado.setText("Aprovado !");
+            textresulatado.setTextColor(Color.BLUE);
+        }
+        if ((media >= 7.51) && (media < 8.00)) {
+            textMedia.setText("Média: " + formatar.format(media));
+            textmediaFinal.setText("Média Final: 7.50");
+            textresulatado.setText("Aprovado !");
+            textresulatado.setTextColor(Color.BLUE);
+        }
+        if ((media >= 8.00) && (media <= 8.50)) {
+            textMedia.setText("Média: " + formatar.format(media));
+            textmediaFinal.setText("Média Final: 8.00");
+            textresulatado.setText("Aprovado !");
+            textresulatado.setTextColor(Color.BLUE);
+        }
+        if ((media >= 8.51) && (media < 9.00)) {
+            textMedia.setText("Média: " + formatar.format(media));
+            textmediaFinal.setText("Média Final: 8.50");
+            textresulatado.setText("Aprovado !");
+            textresulatado.setTextColor(Color.BLUE);
+        }
+        if ((media >= 9.00) && (media <= 9.50)) {
+            textMedia.setText("Média: " + formatar.format(media));
+            textmediaFinal.setText("Média Final: 9.00");
+            textresulatado.setText("Aprovado !");
+            textresulatado.setTextColor(Color.BLUE);
+        }
+        if ((media >= 9.51) && (media < 10.00)) {
+            textMedia.setText("Média: " + formatar.format(media));
+            textmediaFinal.setText("Média Final: 9.50");
+            textresulatado.setText("Aprovado !");
+            textresulatado.setTextColor(Color.BLUE);
+        }
+        if (media == 10.00) {
+            textMedia.setText("Média: " + formatar.format(media));
+            textmediaFinal.setText("Média Final: 10.00");
+            textresulatado.setText("Aprovado !");
+            textresulatado.setTextColor(Color.BLUE);
+        }
+    }
+
+    public void limparCampos(){
+        avaliacao1.setText("");
+        avaliacao2.setText("");
+        discussiva.setText("");
+        avaliacaoFinal.setText("");
+
+        textMedia.setText("");
+        textmediaFinal.setText("");
+        textresulatado.setText("");
     }
 
     @Override
@@ -74,5 +179,10 @@ public class MediaFragmant extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
     }
 }
