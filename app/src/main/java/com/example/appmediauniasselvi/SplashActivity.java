ee8baf36a8d084554cc3dbfab63993d162c02842
clebernascimento <clebertds@gmail.com>
2019-10-25 17:15:14 -0400

    animation splashScreen

--- app/src/main/java/com/example/appmediauniasselvi/SplashActivity.java
+++ app/src/main/java/com/example/appmediauniasselvi/SplashActivity.java
@@ -6,8 +6,14 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class SplashActivity extends AppCompatActivity {
     // Time da splash
@@ -18,6 +24,8 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+
+        StartAnimations();
 //        getSupportActionBar().hide();
 //        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         new Handler().postDelayed(new Runnable() {
@@ -36,5 +44,23 @@ public class SplashActivity extends AppCompatActivity {
                 finish();
             }
         }, SPLASH_TIME_OUT);
+    }
+
+    private void StartAnimations() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        anim.reset();
+        LinearLayout l = findViewById(R.id.main_layout);
+        if (l != null) {
+            l.clearAnimation();
+            l.startAnimation(anim);
+        }
+
+        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        anim.reset();
+        TextView iv = findViewById(R.id.text_splah);
+        if (iv != null) {
+            iv.clearAnimation();
+            iv.startAnimation(anim);
+        }
     }
 }
